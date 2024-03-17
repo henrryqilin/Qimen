e2fc470e09f07bf6af3aaa80af504d38600b28a2
晖洁 陈 <1360322983@qq.com>
2024-03-17 18:02:11 +0800

    改进了主程序的交互和运行逻辑

--- master.py
+++ master.py
@@ -6,13 +6,22 @@ loop = exit()
 
 support = {'1':dipan,'88':loop}
 
-print('欢迎使用奇门遁甲小工具!\n请选择需要起的局')
-print('1:地盘局 88:退出程序')
+print('欢迎使用奇门遁甲小工具!\n请选择需要起的局\n1:地盘局 88:退出程序')
+x = input()
 
 while True:
-    x = input()
     if x in support:
         support[x].Run()
     else:
-        print('不是哥们,咱真没那个能力,能看看上面写啥了吗.')
+        print('不是哥们,咱真没那个能力,这玩意确实不会算.')
+        print('欢迎使用奇门遁甲小工具!\n请选择需要起的局\n1:地盘局 88:退出程序')
+        x = input()
         continue
+
+    print('欢迎使用奇门遁甲小工具!\n请选择需要起的局\n1:地盘局 88:退出程序\n按Enter再开一盘')
+    y = input()
+    if y == '':
+        pass
+    else:
+        x = y
+    
